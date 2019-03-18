2ef17b34b2380a8d97c2ed92c60d9d33dc73bc94
Mateo Tibaquirá <nestormateo@gmail.com>
2019-03-18 17:09:02 -0500

    build: ng add @angular/material

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],

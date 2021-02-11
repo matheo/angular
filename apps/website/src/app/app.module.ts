e7316d8998424d621274896c4bca8c562d782718
Mateo Tibaquira <nestormateo@gmail.com>
2021-02-10 22:12:42 -0500

    build: ng update @angular/cli @angular/core

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled', relativeLinkResolution: 'legacy' })
   ],
   providers: [],
   bootstrap: [AppComponent]

b208ac91526ab083adc56f87a1f9c5c5c122a10f
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-20 19:03:26 -0500

    feat(demos): generalize the datepicker demo typings

--- apps/website/src/app/demos/components/datepicker/datepicker.component.ts
+++ apps/website/src/app/demos/components/datepicker/datepicker.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { DateUnit, MatCalendarCellClassFunction } from '@matheo/datepicker';
+import {
+  DateAdapter,
+  DateUnit,
+  MatCalendarCellClassFunction,
+} from '@matheo/datepicker';
 import { actions, badges } from '../../constants/datepicker.links';
 
 @Component({
@@ -9,7 +13,7 @@ import { actions, badges } from '../../constants/datepicker.links';
   styleUrls: ['./datepicker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DatepickerComponent implements OnInit {
+export class DatepickerComponent<D> implements OnInit {
   // ref links
   actions = actions;
   badges = badges;
@@ -18,45 +22,45 @@ export class DatepickerComponent implements OnInit {
 
   startDate = new Date(1990, 0, 1);
 
-  excludeFilter = (d: Date | null, unit?: DateUnit): boolean => {
-    const hour = (d || new Date()).getHours();
+  excludeFilter = (d: D | null, unit?: DateUnit): boolean => {
+    const hour = this.adapter.getHours(d || this.adapter.today());
     if (unit === 'hour') {
       // prevent office hours from being selected.
       return hour >= 8 && hour <= 18;
     }
 
     if (unit === 'minute') {
-      const minutes = (d || new Date()).getMinutes();
+      const minutes = this.adapter.getMinutes(d || this.adapter.today());
       // we need to limit 18:00
       return hour < 18 || minutes === 0;
     }
 
-    const day = (d || new Date()).getDay();
+    const day = this.adapter.getDayOfWeek(d || this.adapter.today());
     // prevent Saturday and Sunday from being selected.
     return day !== 0 && day !== 6;
   };
 
-  dateClass: MatCalendarCellClassFunction<Date> = (date, view) => {
+  dateClass: MatCalendarCellClassFunction<D> = (date, view) => {
     // Only highligh dates inside the month view.
     if (view === 'month') {
-      const day = date.getDate();
+      const day = this.adapter.getDate(date);
       // Highlight the 1st and 20th day of each month.
       return day === 1 || day === 20 ? 'custom-date-class' : '';
     }
     if (view === 'hour') {
-      const hour = date.getHours();
+      const hour = this.adapter.getHours(date);
       return hour >= 18 || hour < 8 ? 'custom-date-class' : '';
     }
 
     if (view === 'minute') {
-      const minutes = date.getMinutes();
+      const minutes = this.adapter.getMinutes(date);
       return minutes >= 0 && minutes < 30 ? 'custom-date-class' : '';
     }
 
     return '';
   };
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private adapter: DateAdapter<D>) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
